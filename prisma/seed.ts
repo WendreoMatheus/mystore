0345015bdee764496da2aaeac0ab91777b6d4ce4
Wendreo Teixeira <wendreo.teixeira@ambevtech.com.br>
2022-05-30 11:02:59 -0300

    fix: prisma config for migration and seed

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+(async function main() {
+  try {
+    const johnDoe = await prisma.user.upsert({
+      where: { email: 'johndoe@email.com' },
+      update: {},
+      create: {
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+      },
+    })
+
+    console.log('Create 1 user', johnDoe)
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  } finally {
+    await prisma.$disconnect()
+  }
+})()
